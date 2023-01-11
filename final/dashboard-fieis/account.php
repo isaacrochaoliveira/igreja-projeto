5f0a85c9d5c06aea51dd2b9599af5299735e15da
Isaac Victor Rocha Oliveira <isaacvroliveira@gmail.com>
2023-01-10 23:07:29 -0300

    Mais um commit

--- final/dashboard-fieis/account.php
+++ final/dashboard-fieis/account.php
@@ -14,14 +14,27 @@ require_once('../protect.php');
         <title><?= TITLE?></title>
     </head>
     <body>
-        <section class="mx-3 mt-3">
-            <div class="card" style="width: 20rem;">
-                <img src="<?=IMAGEM."images-web/holy-bible.webp"?>" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Planos de Leitura</h5>
-                    <p class="card-text">Participe conosco no nosso plano de leitura anual. Entre quando quizer!</p>
-                    <a href="#" class="btn btn-primary">Participar</a>
+        <section class="mx-3">
+            <div class="row">
+                <div class="col-lg-3 col-6">
+                    <div class="small-box bg-info">
+                        <div class="inner">
+                            <h3>150</h3>
+                            <p>New Orders</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fas fa-shopping-cart"></i>
+                        </div>
+                        <a href="#" class="small-box-footer">
+                            More info <i class="fas fa-arrow-circle-right"></i>
+                        </a>
+                    </div>
                 </div>
+            </div>
+            <div class="mx-3 mt-5">
+                <h1 class="f-family-SourceSerifMediumItalic font-s-4">Edite seu Perfil</h1>
+                <img src="<?=UPLOADS.$_SESSION['imagem']?>" alt="Foto de Perfil do Usuário" width="200" height="200">
+
             </div>
         </section>
     </body>
