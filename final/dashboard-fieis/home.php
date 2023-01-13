781bd4ca13756b94049b092b8d7a290060670e4d
isaacrochaoliveira <120681524+isaacrochaoliveira@users.noreply.github.com>
2023-01-13 13:16:08 -0300

    Sistema para Igrejas

--- final/dashboard-fieis/home.php
+++ final/dashboard-fieis/home.php
@@ -5,34 +5,58 @@ require_once('../config.php');
 require_once('../conexao.php');
 
 ?>
-
+<div class="mt-5 ml-2">
+    <button type="button" class="btn btn-default" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fa-solid fa-plus"></i> Nova Oração</button>
+</div>
 <section class="d-flex mt-60-px mx-3 flex-wrap">
-    
-        <?php
-            $query = $pdo->query("SELECT * FROM oracao JOIN usuarios ON oracao.id_criador = usuarios.id");
-            $res = $query->fetchAll(PDO::FETCH_ASSOC);
-            if (count($res) > 0) { 
-                for ($i = 0; $i < count($res); $i++) {
-                    foreach ($res[$i] as $key => $value) {
-                    }
-                    ?>
-                    <div class="d-flex bg-dark mb-3" style="width: 50%;">
+    <?php
+        $query = $pdo->query("SELECT * FROM oracao JOIN usuarios ON oracao.id_criador = usuarios.id");
+        $res = $query->fetchAll(PDO::FETCH_ASSOC);
+        if (count($res) > 0) { 
+            for ($i = 0; $i < count($res); $i++) {
+                foreach ($res[$i] as $key => $value) {
+                }
+                ?>
+                <div class="d-flex mb-3 py-5 px-3" style="width: 50%;">
                     <?php
                     $titulo = $res[$i]['titulo'];
                     $descricao = $res[$i]['descricao'];
+                    $imagem = $res[$i]['perfil'];
+                    $nome_usuario = $res[$i]['nome'];
+                    $nasc = $res[$i]['nasc'];
+                    $idade = date('Y') - intVal($nasc);
+                    $joelhos_dobrados = $res[$i]['orando'];
                     ?>
 
-                    <img src="<?=IMAGEM."images-web/sem-foto.webp"?>" alt="Sem Foto do Grupo" width="120" height="120">
+                    <img src="<?=UPLOADS.$imagem?>" class="my-auto" alt="Sem Foto de Perfil" width="140" height="140">
                     <div class="ml-2">
-                        <h1><?=$titulo?></h1>
-                        <p><?=$titulo?></p>
+                        <span class="span-style-oracao"><?=$titulo?></span>
+                        <p class="p-style-oracao"><?=$descricao?></p>
+                        <p class="nome-style-oracao"><?=$nome_usuario." - ". $idade. " Anos(s) "?></p>
+                        <p><i class="fa-solid fa-person-praying"></i><?=" Joelhos Dobrados: ".$joelhos_dobrados?></p>
                     </div>
-                    <?php
-                    ?>
-                    </div>
-                    <?php
-                }
+                </div>
+                <?php
             }
-        ?>
-    
+        }
+    ?>
 </section>
+
+<div class="modal fade" id="exampleModal" data-bs-backdrop="static" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                ...
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+        </div>
+    </div>
+</div>
+
