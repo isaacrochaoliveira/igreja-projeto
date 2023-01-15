05edb9083b9b7f92de6c4a3d21ef7e3f2453fbf8
Isaac Victor Rocha Oliveira <isaacvroliveira@gmail.com>
2023-01-15 00:20:57 -0300

    Sistema para Igrejas

--- final/dashboard-fieis/home.php
+++ final/dashboard-fieis/home.php
@@ -18,49 +18,51 @@ $pag = "home";
             for ($i = 0; $i < count($res); $i++) {
                 foreach ($res[$i] as $key => $value) {
                 }
-                ?>
-                <div class="d-flex mb-3 py-5 px-3" style="width: 50%;">
-                    <?php
-                    $id_oracao = $res[$i]['id_pray'];
-                    $titulo = $res[$i]['titulo'];
-                    $descricao = $res[$i]['descricao'];
-                    $imagem = $res[$i]['perfil'];
-                    $nome_usuario = $res[$i]['nome'];
-                    $nasc = $res[$i]['nasc'];
-                    $idade = date('Y') - intVal($nasc);
-                    $joelhos_dobrados = $res[$i]['orando'];
+                if (!($res[$i]['id_criador'] == $_SESSION['id'])) {
                     ?>
+                    <div class="d-flex mb-3 py-5 px-3" style="width: 50%;">
+                        <?php
+                        $id_oracao = $res[$i]['id_pray'];
+                        $titulo = $res[$i]['titulo'];
+                        $descricao = $res[$i]['descricao'];
+                        $imagem = $res[$i]['perfil'];
+                        $nome_usuario = $res[$i]['nome'];
+                        $nasc = $res[$i]['nasc'];
+                        $idade = date('Y') - intVal($nasc);
+                        $joelhos_dobrados = $res[$i]['orando'];
+                        ?>
 
-                    <img src="<?=UPLOADS.$imagem?>" class="my-auto" alt="Sem Foto de Perfil" width="150" height="150">
-                    <div class="ml-2">
-                        <span class="span-style-oracao"><?=$titulo?></span>
-                        <p class="p-style-oracao"><?=$descricao?></p>
-                        <p style="margin-bottom: 0px"><i class="fa-solid fa-person-praying"></i><?=" Joelhos Dobrados: ".$joelhos_dobrados?></p>
-                        <p style="margin-bottom: 0px;">Categoria: 
-                            <?php
-                                $query_cat = $pdo->query("SELECT * FROM oracao_relacionada_com_a_categoria as oc JOIN categorias ON oc.id_categoria = categorias.id_cat JOIN oracao ON oracao.id_pray = oc.id_oracao");
-                                $res_cat = $query_cat->fetchAll(PDO::FETCH_ASSOC);
-                                if (count($res_cat) > 0) {
-                                    for ($c = 0; $c < count($res_cat); $c++) {
-                                        foreach ($res_cat[$c] as $key => $v) {
+                        <img src="<?=UPLOADS.$imagem?>" class="my-auto" alt="Sem Foto de Perfil" width="150" height="150">
+                        <div class="ml-2">
+                            <span class="span-style-oracao"><?=$titulo?></span>
+                            <p class="p-style-oracao"><?=$descricao?></p>
+                            <p style="margin-bottom: 0px"><i class="fa-solid fa-person-praying"></i><?=" Joelhos Dobrados: ".$joelhos_dobrados?></p>
+                            <p style="margin-bottom: 0px;">Categoria: 
+                                <?php
+                                    $query_cat = $pdo->query("SELECT * FROM oracao_relacionada_com_a_categoria as oc JOIN categorias ON oc.id_categoria = categorias.id_cat JOIN oracao ON oracao.id_pray = oc.id_oracao");
+                                    $res_cat = $query_cat->fetchAll(PDO::FETCH_ASSOC);
+                                    if (count($res_cat) > 0) {
+                                        for ($c = 0; $c < count($res_cat); $c++) {
+                                            foreach ($res_cat[$c] as $key => $v) {
+                                            }
+                                            if ($res_cat[$c]['id_oracao'] == $id_oracao) {
+                                                $categoria = $res_cat[$c]['categorias'];
+                                                echo $categoria.". ";
+                                            }
                                         }
-                                        if ($res_cat[$c]['id_oracao'] == $id_oracao) {
-                                            $categoria = $res_cat[$c]['categorias'];
-                                            echo $categoria.". ";
-                                        }
+                                    } else {
+                                        ?>
+                                        Sem Categoria
+                                        <?php
                                     }
-                                } else {
-                                    ?>
-                                    Sem Categoria
-                                    <?php
-                                }
 
-                            ?> 
-                        </p>
-                        <p class="nome-style-oracao"><?=$nome_usuario." - ". $idade. " Anos(s) "?></p>
+                                ?> 
+                            </p>
+                            <p class="nome-style-oracao"><?=$nome_usuario." - ". $idade. " Anos(s) "?></p>
+                        </div>
                     </div>
-                </div>
-                <?php
+                    <?php
+                }
             }
         }
     ?>
@@ -70,7 +72,7 @@ $pag = "home";
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h1 class="modal-title fs-5" id="exampleModalLabel">Sua Oração</h1>
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Sua Oração - <a href="index.php?pag=pedidos-de-oracoes" target="_blank">Ver todas as minhas orações</a></h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <form action="" method="POST">
@@ -181,7 +183,7 @@ $pag = "home";
                     </div>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" name="btn-fechar-pedido" id="btn-fechar-pedido">Voltar</button>
                     <button type="button" class="btn btn-success" name="btn-salvar-pedido" id="btn-salvar-pedido">Adicionar</button>
                 </div>
             </form>
@@ -201,6 +203,7 @@ $pag = "home";
                 dataType: "text",
                 success: function(msg) {
                     alert(msg);
+                    $('#btn-fechar-pedido').click();
                 }
             })
         })
--- final/dashboard-fieis/home/inserir_oracao.php
+++ final/dashboard-fieis/home/inserir_oracao.php
@@ -11,39 +11,39 @@ $categorias3 = addslashes($_POST['cat3']);
 $categorias4 = addslashes($_POST['cat4']);
 
 
-$res = $pdo->prepare("INSERT INTO oracao SET id_criador = ':id_criador', titulo = ':titulo', descricao = ':descricao', orando = ':orando'");
+$res = $pdo->prepare("INSERT INTO oracao SET id_criador = :id_criador, titulo = :titulo, descricao = :descricao, orando = :orando");
 $res->bindValue(':id_criador', $_SESSION['id']);
 $res->bindValue(':titulo', $titulo);
 $res->bindValue(':descricao', $descricao);
 $res->bindValue(':orando', 0);
 if ($res->execute()) {
-	$query = $pdo->query("SELECT * FROM oracao ORDER BY id DESC");
-	$res = $query->fetchAll(PDO::FETCH_ASSOC);
-	if (count($res) > 0) {
-		$id_oracao = $res[0]['id'];
+	$query = $pdo->query("SELECT * FROM oracao ORDER BY id_pray DESC");
+	$res_ = $query->fetchAll(PDO::FETCH_ASSOC);
+	if (count($res_) > 0) {
+		$id_oracao = $res_[0]['id_pray'];
 		if (!empty($categorias1)) {
-			$res_cat = $pdo->prepare("INSERT INTO oracao_relacionada_com_a_categoria SET id_oracao = ':id_oracao', id_categoria = ':id_cat'");
-			$res->bindValue(':id_oracao', $id_oracao);
-			$res->bindValue(':id_cat', $categorias1);
-			$res->execute();
+			$res_cat = $pdo->prepare("INSERT INTO oracao_relacionada_com_a_categoria SET id_oracao = :id_oracao, id_categoria = :id_cat");
+			$res_cat->bindValue(':id_oracao', $id_oracao);
+			$res_cat->bindValue(':id_cat', $categorias1);
+			$res_cat->execute();
 		} 
 		if (!empty($categorias2)) {
-			$res_cat = $pdo->prepare("INSERT INTO oracao_relacionada_com_a_categoria SET id_oracao = ':id_oracao', id_categoria = ':id_cat'");
-			$res->bindValue(':id_oracao', $id_oracao);
-			$res->bindValue(':id_cat', $categorias2);
-			$res->execute();
+			$res_cat = $pdo->prepare("INSERT INTO oracao_relacionada_com_a_categoria SET id_oracao = :id_oracao, id_categoria = :id_cat");
+			$res_cat->bindValue(':id_oracao', $id_oracao);
+			$res_cat->bindValue(':id_cat', $categorias2);
+			$res_cat->execute();
 		}
 		if (!empty($categorias3)) {
-			$res_cat = $pdo->prepare("INSERT INTO oracao_relacionada_com_a_categoria SET id_oracao = ':id_oracao', id_categoria = ':id_cat'");
-			$res->bindValue(':id_oracao', $id_oracao);
-			$res->bindValue(':id_cat', $categorias1);
-			$res->execute();
+			$res_cat = $pdo->prepare("INSERT INTO oracao_relacionada_com_a_categoria SET id_oracao = :id_oracao, id_categoria = :id_cat");
+			$res_cat->bindValue(':id_oracao', $id_oracao);
+			$res_cat->bindValue(':id_cat', $categorias3);
+			$res_cat->execute();
 		}
 		if (!empty($categorias4)) {
-			$res_cat = $pdo->prepare("INSERT INTO oracao_relacionada_com_a_categoria SET id_oracao = ':id_oracao', id_categoria = ':id_cat'");
-			$res->bindValue(':id_oracao', $id_oracao);
-			$res->bindValue(':id_cat', $categorias4);
-			$res->execute();
+			$res_cat = $pdo->prepare("INSERT INTO oracao_relacionada_com_a_categoria SET id_oracao = :id_oracao, id_categoria = :id_cat");
+			$res_cat->bindValue(':id_oracao', $id_oracao);
+			$res_cat->bindValue(':id_cat', $categorias4);
+			$res_cat->execute();
 		}
 	}
 	echo "Oração inserida com Sucesso! Vamos vincular a sua foto com a oração.";
