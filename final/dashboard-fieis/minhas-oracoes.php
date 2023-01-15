66d073adb6e547361fe15bf737044ba50f7a2f54
Isaac Victor Rocha Oliveira <isaacvroliveira@gmail.com>
2023-01-15 00:28:37 -0300

    Update minhas-oracoes.php

--- final/dashboard-fieis/minhas-oracoes.php
+++ final/dashboard-fieis/minhas-oracoes.php
@@ -31,9 +31,9 @@ require_once('../conexao.php');
                             <div class="d-flex justify-content-between">
                                 <span class="span-style-oracao"><?=$titulo?></span>
                                 <div class="d-flex flex-wrap">
-                                    <button style="border: none; background-color: white; margin: 0px 3px" name="btn_emproposito" id="btn_emprsoposito_<?=$id_oracao?>">
+                                    <a href="index.php?pag=minhas-oracoes&id_pray_edit=<?=$id_oracao?>" style="border: none; background-color: white; margin: 0px 3px" name="btn_emproposito" id="btn_emprsoposito_<?=$id_oracao?>">
                                         <i class="fa-solid fa-file" style="font-size: 20px;" title="Editar Oração"></i>
-                                    </button>
+                                    </a>
                                     <button onclick="ExcluirOracao(<?=$id_oracao?>)" style="border: none; color: red; background-color: white; margin: 0px 3px" onclick="">
                                         <i class="fa-solid fa-trash" style="font-size: 20px;" title="Excluir Oração"></i>
                                     </button>
@@ -78,28 +78,152 @@ require_once('../conexao.php');
     ?>
 </section>
 
-<div class="modal fade" id="modalPrayEdit" tabindex="-1" data-bs-backdrop="static" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="modalPrayEdit" data-bs-backdrop="static" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Sua Oração</h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <?php
+                    $query = $pdo->query("SELECT * FROM oracao WHERE id_pray = '$pray'");
+                    $res = $query->fetchAll(PDO::FETCH_ASSOC);
+                    if (count($res) > 0) {
+
+                    }
+                ?>
             </div>
-            <div class="modal-body">
-                ...
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
-            </div>
+            <form action="" method="POST">
+                <div class="modal-body">
+                    <div class="col mb-2">
+                        <label for="title">Título da sua Oração</label>
+                        <input type="text" name="titulo" id="titulo" class="form-control">
+                    </div>
+                    <div class="col mb-2">
+                        <label for="desc">Escreva aqui sua oração</label>
+                        <textarea rows="3" cols="4" class="form-control" name="desc" id="desc"></textarea>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6 mb-3">
+                            <label for="cat1">1º Categorias</label>
+                            <select class="form-select" name="cat1">
+                                <?php
+                                    $query_cate = $pdo->query("SELECT * FROM categorias");
+                                    $res_cate = $query_cate->fetchAll(PDO::FETCH_ASSOC);
+                                     ?>
+                                    <option value="" selected>Selecione Um</option>
+                                    <?php
+                                    if (count($res_cate) > 0) {
+                                        for ($i = 0; $i < count($res_cate); $i++) {
+                                            foreach ($res_cate[$i] as $key => $value) {
+                                            }
+                                            $id = $res_cate[$i]['id_cat'];
+                                            $categorias = $res_cate[$i]['categorias'];
+                                            ?>
+                                            <option value="<?php echo $id ?>"><?php echo $categorias?></option>
+                                            <?php
+                                        }
+                                    }
+                                ?>
+                            </select>
+                        </div>
+                        <div class="col-md-6 mb-3">
+                            <label for="cat2">2º Categoria</label>
+                            <select class="form-select" name="cat2">
+                                 <?php
+                                    $query_cate = $pdo->query("SELECT * FROM categorias");
+                                    $res_cate = $query_cate->fetchAll(PDO::FETCH_ASSOC);
+                                     ?>
+                                    <option value="" selected>Selecione Um</option>
+                                    <?php
+                                    if (count($res_cate) > 0) {
+                                        for ($i = 0; $i < count($res_cate); $i++) {
+                                            foreach ($res_cate[$i] as $key => $value) {
+                                            }
+                                            $id = $res_cate[$i]['id_cat'];
+                                            $categorias = $res_cate[$i]['categorias'];
+                                            ?>
+                                            <option value="<?php echo $id ?>"><?php echo $categorias?></option>
+                                            <?php
+                                        }
+                                    }
+                                ?>                               
+                            </select>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6 mb-3">
+                            <label for="cat3">3º Categorias</label>
+                            <select class="form-select" name="cat3">
+                                <?php
+                                    $query_cate = $pdo->query("SELECT * FROM categorias");
+                                    $res_cate = $query_cate->fetchAll(PDO::FETCH_ASSOC);
+                                     ?>
+                                    <option value="" selected>Selecione Um</option>
+                                    <?php
+                                    if (count($res_cate) > 0) {
+                                        for ($i = 0; $i < count($res_cate); $i++) {
+                                            foreach ($res_cate[$i] as $key => $value) {
+                                            }
+                                            $id = $res_cate[$i]['id_cat'];
+                                            $categorias = $res_cate[$i]['categorias'];
+                                            ?>
+                                            <option value="<?php echo $id ?>"><?php echo $categorias?></option>
+                                            <?php
+                                        }
+                                    }
+                                ?>
+                            </select>
+                        </div>
+                        <div class="col-md-6 mb-3">
+                            <label for="cat4">4º Categoria</label>
+                            <select class="form-select" name="cat4">
+                                 <?php
+                                    $query_cate = $pdo->query("SELECT * FROM categorias");
+                                    $res_cate = $query_cate->fetchAll(PDO::FETCH_ASSOC);
+                                    ?>
+                                    <option value="" selected>Selecione Um</option>
+                                    <?php
+                                    if (count($res_cate) > 0) {
+                                        for ($i = 0; $i < count($res_cate); $i++) {
+                                            foreach ($res_cate[$i] as $key => $value) {
+                                            }
+                                            $id = $res_cate[$i]['id_cat'];
+                                            $categorias = $res_cate[$i]['categorias'];
+                                            ?>
+                                            <option value="<?php echo $id ?>"><?php echo $categorias?></option>
+                                            <?php
+                                        }
+                                    }
+                                ?>                               
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <input type="text" name="id_oracao_edit_id" id="id_oracao_edit_id">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" name="btn-fechar-pedido" id="btn-fechar-pedido">Voltar</button>
+                    <button type="button" class="btn btn-success" name="btn-salvar-pedido" id="btn-salvar-pedido">Adicionar</button>
+                </div>
+            </form>
         </div>
     </div>
 </div>
 
+<?php 
+if (!isset($_GET['id_pray_edit'])) {
+    echo "<script>$('#modalPrayEdit').modal('show')</script";
+}
+    
+?>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
-
+<script type="text/javascript">
+    function EditarOracao(id) {
+        $('#id_oracao_edit_id').val(id);
+        $('#modalPrayEdit').modal('show');
+    }
+</script>
 
 <script type="text/javascript">
     function ExcluirOracao(id) {
