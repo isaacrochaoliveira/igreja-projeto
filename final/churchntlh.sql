181be93b32ab0afe366b4254d47350b3d6d926b6
Isaac Victor Rocha Oliveira <isaacvroliveira@gmail.com>
2023-01-30 13:35:46 -0300

    Update churchntlh.sql

--- final/churchntlh.sql
+++ final/churchntlh.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Tempo de geração: 20-Jan-2023 às 23:05
+-- Tempo de geração: 30-Jan-2023 às 16:35
 -- Versão do servidor: 8.0.31
 -- versão do PHP: 8.0.26
 
@@ -24,6 +24,28 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `anotacoes`
+--
+
+DROP TABLE IF EXISTS `anotacoes`;
+CREATE TABLE IF NOT EXISTS `anotacoes` (
+  `id` tinyint NOT NULL AUTO_INCREMENT,
+  `id_group` tinyint DEFAULT NULL,
+  `anotacao` text COLLATE utf8mb4_general_ci,
+  PRIMARY KEY (`id`),
+  KEY `id_group` (`id_group`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `anotacoes`
+--
+
+INSERT INTO `anotacoes` (`id`, `id_group`, `anotacao`) VALUES
+(10, 36, 'Mais uma');
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `cargos`
 --
 
@@ -32,7 +54,7 @@ CREATE TABLE IF NOT EXISTS `cargos` (
   `id_cargo` tinyint NOT NULL AUTO_INCREMENT,
   `cargo` varchar(100) NOT NULL,
   PRIMARY KEY (`id_cargo`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Extraindo dados da tabela `cargos`
@@ -65,7 +87,7 @@ CREATE TABLE IF NOT EXISTS `categorias` (
   `id_cat` tinyint NOT NULL AUTO_INCREMENT,
   `categorias` varchar(50) NOT NULL,
   PRIMARY KEY (`id_cat`)
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Extraindo dados da tabela `categorias`
@@ -107,7 +129,7 @@ CREATE TABLE IF NOT EXISTS `citacoes` (
   `author` varchar(50) NOT NULL,
   `citacao` text,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Extraindo dados da tabela `citacoes`
@@ -116,6 +138,37 @@ CREATE TABLE IF NOT EXISTS `citacoes` (
 INSERT INTO `citacoes` (`id`, `author`, `citacao`) VALUES
 (1, 'Paul Washer', 'A beleza da mulher não está no seu aspecto físico, mas no temor e na obediência a Deus'),
 (2, 'Billy Graham', 'Deus nos deu duas mãos: uma para receber e outra para dar. Não somos cisternas feitas para acumular, somos canais feitos para compartilhar');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `comentarios_grupos`
+--
+
+DROP TABLE IF EXISTS `comentarios_grupos`;
+CREATE TABLE IF NOT EXISTS `comentarios_grupos` (
+  `id_comment` tinyint NOT NULL AUTO_INCREMENT,
+  `id_comentador` tinyint DEFAULT NULL,
+  `id_grupo` tinyint DEFAULT NULL,
+  `comentario` text COLLATE utf8mb4_general_ci,
+  `pessoas_curtiram` int DEFAULT NULL,
+  `data_comment` date DEFAULT NULL,
+  `hora_comment` time DEFAULT NULL,
+  PRIMARY KEY (`id_comment`),
+  KEY `id_comentador` (`id_comentador`),
+  KEY `id_grupo` (`id_grupo`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `comentarios_grupos`
+--
+
+INSERT INTO `comentarios_grupos` (`id_comment`, `id_comentador`, `id_grupo`, `comentario`, `pessoas_curtiram`, `data_comment`, `hora_comment`) VALUES
+(1, 10, 36, 'Olá, Mundo!', 0, '2023-01-30', '13:46:30'),
+(2, 10, 36, 'Meu 2º Comentário!', 0, '2023-01-30', '14:21:03'),
+(3, 8, 36, 'Deixei meu comentário aqui', 0, '2023-01-30', '13:29:37'),
+(4, 8, 2, 'Eu não sou mulher, mas sorte terá aquela que me encontrar!', 0, '2023-01-30', '13:30:52'),
+(5, 2, 36, 'Nossa que forte esse grupo! Acordarei todo dia!', 0, '2023-01-30', '13:31:50');
 
 -- --------------------------------------------------------
 
@@ -131,7 +184,7 @@ CREATE TABLE IF NOT EXISTS `emproposito_na_oracao` (
   PRIMARY KEY (`id`),
   KEY `id_usuario` (`id_usuario`),
   KEY `id_oracao` (`id_oracao`)
-) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Extraindo dados da tabela `emproposito_na_oracao`
@@ -143,7 +196,13 @@ INSERT INTO `emproposito_na_oracao` (`id`, `id_usuario`, `id_oracao`) VALUES
 (24, 4, 23),
 (25, NULL, 23),
 (32, 11, 24),
-(33, 11, 23);
+(33, 11, 23),
+(34, 4, 9),
+(35, 12, 4),
+(36, 8, 9),
+(37, 10, 4),
+(39, 10, 9),
+(40, 10, 22);
 
 -- --------------------------------------------------------
 
@@ -162,20 +221,22 @@ CREATE TABLE IF NOT EXISTS `grupos_de_oracao` (
   `pessoas_part` int DEFAULT '0',
   `criado_em` date DEFAULT NULL,
   `hora_criado_em` time DEFAULT NULL,
-  `fechado_em` datetime DEFAULT NULL,
   `ativo` enum('S','N') DEFAULT 'S',
+  `fechadoEm` date DEFAULT NULL,
+  `hora_fechadoEm` time DEFAULT NULL,
   PRIMARY KEY (`id_group`),
   KEY `id_criador` (`id_criador`),
   KEY `id_licenca` (`id_licenca`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Extraindo dados da tabela `grupos_de_oracao`
 --
 
-INSERT INTO `grupos_de_oracao` (`id_group`, `id_criador`, `id_licenca`, `logo`, `title`, `descricao`, `pessoas_part`, `criado_em`, `hora_criado_em`, `fechado_em`, `ativo`) VALUES
-(1, 8, NULL, 'grupo-de-intercessao.png', 'Grupo de Intercessão', 'Buscamos a face de Deus dia após dia, a bíblia nos fala orai sem cesar. Por que nós oramos? Simples! Você não pediria um ajuda para a única pessoa que consegue fazer aquilo que você tanto deseja?', 18, '2023-01-16', '00:53:32', '0000-00-00 00:00:00', 'S'),
-(2, 10, 1, '63ac6ec8ee488.png', 'Oração para as Solteiras', 'Eu vim para orar pelo meu futuro marido, porque eu vim com sapatinho de fogo', 15, '2023-01-18', '23:52:15', '0000-00-00 00:00:00', 'S');
+INSERT INTO `grupos_de_oracao` (`id_group`, `id_criador`, `id_licenca`, `logo`, `title`, `descricao`, `pessoas_part`, `criado_em`, `hora_criado_em`, `ativo`, `fechadoEm`, `hora_fechadoEm`) VALUES
+(1, 8, NULL, '63d1cb40584b4.jpg', 'Grupo de Intercessão', 'Buscamos a face de Deus dia após dia, a bíblia nos fala orai sem cesar. Por que nós oramos? Simples! Você não pediria um ajuda para a única pessoa que consegue fazer aquilo que você tanto deseja?', 18, '2023-01-16', '00:53:32', 'S', NULL, NULL),
+(2, 10, 1, '63ac6ec8ee488.png', 'Oração para as Solteiras', 'Eu vim para orar pelo meu futuro marido, porque eu vim com sapatinho de fogo (nós viemos)', 15, '2023-01-18', '23:52:15', 'S', NULL, NULL),
+(36, 10, 2, '63d54329a96ec.jpg', 'Sabedoria e Prudência', 'Pv - Eu amo os que me amam, e os que de madrugada me buscam me acharão', 0, '2023-01-28', '13:37:01', 'S', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -189,14 +250,15 @@ CREATE TABLE IF NOT EXISTS `licenca` (
   `nome_da_licenca` varchar(200) COLLATE utf8mb4_general_ci NOT NULL,
   `descricao_da_licenca` text COLLATE utf8mb4_general_ci,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Extraindo dados da tabela `licenca`
 --
 
 INSERT INTO `licenca` (`id`, `nome_da_licenca`, `descricao_da_licenca`) VALUES
-(1, 'EVAN', 'A Licença EVAN é baseada na estutura de devocional completo. Onde nós, evangélicos não acreditamos que existe um horario expecifíco para orar');
+(1, 'EVAN', 'A Licença EVAN é baseada na estutura de devocional completo. Onde nós, evangélicos não acreditamos que existe um horario expecifíco para orar'),
+(2, 'NONE', 'Livre de Qualquer Preocupação');
 
 -- --------------------------------------------------------
 
@@ -213,17 +275,17 @@ CREATE TABLE IF NOT EXISTS `oracao` (
   `orando` int DEFAULT '0',
   PRIMARY KEY (`id_pray`),
   KEY `id_criador` (`id_criador`)
-) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Extraindo dados da tabela `oracao`
 --
 
 INSERT INTO `oracao` (`id_pray`, `id_criador`, `titulo`, `descricao`, `orando`) VALUES
-(4, 8, 'Varoa', 'A Mulher Prudente vem do Senhor. Me ajudem em oração', 122),
-(9, 10, 'Varão', 'Esposa, obedeça ao seu marido, como você obedece ao Senhor. Pois o marido tem autoridade sobre a esposa, assim como Cristo tem autoridade sobre a Igreja', 8),
+(4, 8, 'Varoa', 'A Mulher Prudente vem do Senhor. Me ajudem em oração', 124),
+(9, 10, 'Varão', 'Esposa, obedeça ao seu marido, como você obedece ao Senhor. Pois o marido tem autoridade sobre a esposa, assim como Cristo tem autoridade sobre a Igreja', 0),
 (21, 4, 'Obrigado!', 'Me ajudem a agradecer meu Deus por tudo. Obrigado', 1),
-(22, 9, 'Aqui é a Paula', 'Estou atualmente querendo entrar em um emprego, mas preciso saber se é da vontade do meu Pai. Me ajudem em oração, Obrigado!', 5),
+(22, 9, 'Aqui é a Paula', 'Estou atualmente querendo entrar em um emprego, mas preciso saber se é da vontade do meu Pai. Me ajudem em oração, Obrigado!', 6),
 (23, 11, 'Oração', 'Estou em processo de um relacionamento pessoal\r\n', 4),
 (24, 11, 'Salmos 125:1', 'Me ajude a orar a ficar mais parecido com o Monte Sião. Além disso, quando vocês orarem ao meu favor, automaticamente Deus vai pelejar ao seu favor', 0);
 
@@ -241,7 +303,7 @@ CREATE TABLE IF NOT EXISTS `oracao_relacionada_com_a_categoria` (
   PRIMARY KEY (`id`),
   KEY `id_oracao` (`id_oracao`),
   KEY `id_categoria` (`id_categoria`)
-) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Extraindo dados da tabela `oracao_relacionada_com_a_categoria`
@@ -284,7 +346,7 @@ CREATE TABLE IF NOT EXISTS `participando_do_grupo` (
   `id_usuario` tinyint DEFAULT NULL,
   `id_grupo` tinyint DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -308,14 +370,18 @@ CREATE TABLE IF NOT EXISTS `regras_do_grupo` (
   `_regras10` text COLLATE utf8mb4_general_ci,
   PRIMARY KEY (`id`),
   KEY `id_grupo` (`id_grupo`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Extraindo dados da tabela `regras_do_grupo`
 --
 
 INSERT INTO `regras_do_grupo` (`id`, `id_grupo`, `_regras1`, `_regras2`, `_regras3`, `_regras4`, `_regras5`, `_regras6`, `_regras7`, `_regras8`, `_regras9`, `_regras10`) VALUES
-(1, 2, 'Ler Mais', 'Orar Mais', 'Seja a sua melhor versão', 'Seja Emocionalmente forte', 'Vamos acordar 6:00 para orar', 'Exclua os contatinhos', 'Não fale com os te desviam do camihno', 'Vamos Jejuar 1 vez por semana', 'todas vão orar nesse horario', NULL);
+(1, 2, 'Ler Mais', 'Olá Mundo!', 'Anotar as qualidades que você quer no seu ungido!', 'Olá', 'Olá', 'Olá', 'Olá', 'Olá', 'Olá', 'Olá'),
+(18, 2, 'Ler Mais', 'Olá Mundo!', 'Anotar as qualidades que você quer no seu ungido!', 'Olá', 'Olá', 'Olá', 'Olá', 'Olá', 'Olá', 'Olá'),
+(19, 2, 'Ler Mais', 'Olá Mundo!', 'Anotar as qualidades que você quer no seu ungido!', 'Olá', 'Olá', 'Olá', 'Olá', 'Olá', 'Olá', 'Olá'),
+(20, 2, 'Ler Mais', 'Olá Mundo!', 'Anotar as qualidades que você quer no seu ungido!', 'Olá', 'Olá', 'Olá', 'Olá', 'Olá', 'Olá', 'Olá'),
+(21, 36, 'Orar de Madrugada', 'Ler de Madrugada', 'Às 3:00', 'Um dia sim e um não (todo dia, se você der conta))', 'Jejum 1 por Semana', NULL, NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -342,7 +408,7 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   UNIQUE KEY `cpf` (`cpf`),
   UNIQUE KEY `email` (`email`),
   KEY `id_cargo` (`id_cargo`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Extraindo dados da tabela `usuarios`
@@ -359,11 +425,25 @@ INSERT INTO `usuarios` (`id`, `id_cargo`, `nome`, `nasc`, `sexo`, `cpf`, `email`
 (8, 2, 'Marcos', '2004-07-23', 'M', '142154541010', 'negao_rocha@gmail.com', 'negao', '63bb214604be3.jpg', 'Casado', 'S', '08:00:00', '18:00:00'),
 (9, 7, 'Paula', '2003-04-12', 'F', '77777777777', 'paulinha@gmail.com', 'ppp123', 'sem-foto.jpg', 'Namorando', 'N', '00:00:00', '00:00:00'),
 (10, 12, 'CristHelly', '2013-06-12', 'F', '12213242352', 'crist@gmail.com', '1218', '63c375842db86.jpg', 'Namorando', 'N', '00:00:00', '00:00:00'),
-(11, 11, 'Latrel', '2004-07-23', 'M', '21316465464', 'latrel@gmail.com', 'latrel', '63c6adee0eb03.jpg', 'Namorando', 'S', '08:00:00', '18:30:00');
+(11, 11, 'Latrel', '2004-07-23', 'M', '21316465464', 'latrel@gmail.com', 'latrel', '63c6adee0eb03.jpg', 'Namorando', 'S', '08:00:00', '18:30:00'),
+(12, 6, 'Mickaelle', '1993-10-05', 'F', '291804933019', 'mick@gmail.com', 'protegida', '63cc4531b77ca.png', 'Solteiro', 'S', '09:00:00', '18:30:00');
 
 --
 -- Restrições para despejos de tabelas
 --
+
+--
+-- Limitadores para a tabela `anotacoes`
+--
+ALTER TABLE `anotacoes`
+  ADD CONSTRAINT `anotacoes_ibfk_1` FOREIGN KEY (`id_group`) REFERENCES `grupos_de_oracao` (`id_group`);
+
+--
+-- Limitadores para a tabela `comentarios_grupos`
+--
+ALTER TABLE `comentarios_grupos`
+  ADD CONSTRAINT `comentarios_grupos_ibfk_1` FOREIGN KEY (`id_comentador`) REFERENCES `usuarios` (`id`),
+  ADD CONSTRAINT `comentarios_grupos_ibfk_2` FOREIGN KEY (`id_grupo`) REFERENCES `grupos_de_oracao` (`id_group`);
 
 --
 -- Limitadores para a tabela `emproposito_na_oracao`
