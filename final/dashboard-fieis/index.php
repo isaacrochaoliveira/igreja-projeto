1372e756acd1b7ea8c21c3412f94fb621040505f
Isaac Victor Rocha Oliveira <isaacvroliveira@gmail.com>
2023-01-31 06:45:51 -0300

    Sistema para Igrejas

--- final/dashboard-fieis/index.php
+++ final/dashboard-fieis/index.php
@@ -30,7 +30,6 @@
         <title><?= TITLE?></title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
-        <link rel="stylesheet" href="../assets/plugins/fullcalendar/main.css">
         <link rel="stylesheet" href="../assets/styles/dashboard.css">
         <link rel="stylesheet" href="../assets/styles/adminlte.min.css">
     </head>
@@ -188,6 +187,7 @@
         @include_once("home.php");
     }
     ?>
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     <?php
     if (isset($_GET['jointogroup'])) {
