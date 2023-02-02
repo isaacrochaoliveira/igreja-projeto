fde7b896da31dfe53e1e10a07176ff9560b9293e
Isaac Victor Rocha Oliveira <isaacvroliveira@gmail.com>
2023-02-02 06:25:23 -0300

    Update inserir_pastor.php

--- final/dashboard-fieis/home/inserir_pastor.php
+++ final/dashboard-fieis/home/inserir_pastor.php
@@ -25,7 +25,36 @@ if ($nascionalidade == "") {
 }
 
 if ($id_pas == "") {
-    $res = $pdo->prepare("INSERT INTO pastores SET nome_pas = ")
+    $res = $pdo->prepare("INSERT INTO pastores SET nome_pas = :nome_pas, email_pas = :email_pas, tempo_pas = :tempo_pas, telefone_pas = :telefone_pas, nascionalidade_pas = :nascionalidade_pas, profissao_pas = :profissao_pas, ministerio_pas = :ministerio_pas, casado_pas = :casado_pas, qunt_casado_pas = :qunt_casado_pas, qunt_membros_pas = :qunt_membros_pas, data_cadastro_pas = :data_cadastro_pas, hora_cadastro_pas");
+    $res->bindValue(':nome_pas', $nome);
+    $res->bindValue(':email_pas', $email);
+    $res->bindValue(':tempo_pas', $tempo);
+    $res->bindValue(':telefone_pas', $telefone);
+    $res->bindValue(':nascionalidade_pas', $nascionalidade);
+    $res->bindValue(':profissao_pas', $profissao);
+    $res->bindValue(':ministerio_pas', $ministerio);
+    $res->bindValue(':casado_pas', $casado);
+    $res->bindValue(':qunt_casado_pas', $qunt_casado);
+    $res->bindValue(':qunt_membros', $qunt_membros);
+} else {
+    $res = $pdo->prepare("UPDATE pastores SET nome_pas = :nome_pas, email_pas = :email_pas, tempo_pas = :tempo_pas, telefone_pas = :telefone_pas, nascionalidade_pas = :nascionalidade_pas, profissao_pas = :profissao_pas, ministerio_pas = :ministerio_pas, casado_pas = :casado_pas, qunt_casado_pas = :qunt_casado_pas, qunt_membros_pas = :qunt_membros_pas, data_cadastro_pas = :data_cadastro_pas, hora_cadastro_pas WHERE id_pas = :id_pas");
+    $res->bindValue(':id_pas', $id_pas);
+    $res->bindValue(':nome_pas', $nome);
+    $res->bindValue(':email_pas', $email);
+    $res->bindValue(':tempo_pas', $tempo);
+    $res->bindValue(':telefone_pas', $telefone);
+    $res->bindValue(':nascionalidade_pas', $nascionalidade);
+    $res->bindValue(':profissao_pas', $profissao);
+    $res->bindValue(':ministerio_pas', $ministerio);
+    $res->bindValue(':casado_pas', $casado);
+    $res->bindValue(':qunt_casado_pas', $qunt_casado);
+    $res->bindValue(':qunt_membros', $qunt_membros);
+}
+
+if ($res->execute()) {
+    echo "Pastor Cadastro com Sucesso!";
+} else {
+    echo "Erro na Inserção!";
 }
 
 ?>
