fb0fc589d6a6732051ab6a8658ffa5fce104fd76
Isaac Victor Rocha Oliveira <isaacvroliveira@gmail.com>
2023-05-13 00:27:07 -0300

    SPI

--- final/config.php
+++ final/config.php
@@ -3,6 +3,7 @@
 define('URL_BASE', "http://$_SERVER[HTTP_HOST]/projeto-igreja/igreja-projeto/final/");
 define('IMAGEM', "http://$_SERVER[HTTP_HOST]/projeto-igreja/igreja-projeto/final/assets/img/");
 define('UPLOADS', "http://$_SERVER[HTTP_HOST]/projeto-igreja/igreja-projeto/final/assets/img/fotos/");
+define('JS', "http://$_SERVER[HTTP_HOST]/projeto-igreja/igreja-projeto/final/assets/js/");
 define('SERVER', 'localhost');
 define('DBNAME', 'churchntlh');
 define('ROOT', 'root');
